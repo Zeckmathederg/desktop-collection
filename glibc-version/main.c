93f1ab2f6f63037be89abafc88a6e4e5d591a73c
Zeckmathederg <zeckmaisreal@gmail.com>
2024-06-15 15:20:55 -0600

    Made glibc-version even more simple...

--- glibc-version/main.c
+++ glibc-version/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-	const char* glibcVersion = gnu_get_libc_version();
-	printf("/usr/lib/libc.so.6 is glibc-%s\n",glibcVersion);
+	//const char* glibcVersion = gnu_get_libc_version();
+	printf("/usr/lib/libc.so.6 is glibc-%s\n",gnu_get_libc_version());
 	return 0;
 }
